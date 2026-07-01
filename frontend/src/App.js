50181d45e0725f71415eed2d5d3ff92143d9a6bb
Praveena <praveenprakash02@gmail.com>
2026-07-01 13:20:47 +0530

    Integrated React dashboard with FastAPI backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,9 @@ import API from "./api/api";
 
 function App() {
 const [loads, setLoads] = useState([]);
+const [dashboard, setDashboard] = useState({});
+useEffect(() => {
 
- useEffect(() => {
   API.get("/loads")
     .then((response) => {
       console.log("DATA:", response.data);
@@ -13,6 +14,16 @@ const [loads, setLoads] = useState([]);
     .catch((error) => {
       console.error(error);
     });
+
+  API.get("/dashboard")
+    .then((response) => {
+      console.log("Dashboard:", response.data);
+      setDashboard(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
 }, []);
 
   return (
@@ -21,7 +32,36 @@ const [loads, setLoads] = useState([]);
       <h1 className="text-center mb-4">
         Smart Grid Dashboard
       </h1>
+         <div className="row mb-4">
 
+  <div className="col-md-4">
+    <div className="card text-white bg-primary">
+      <div className="card-body">
+        <h5>Total Sectors</h5>
+        <h2>{dashboard.total_sectors}</h2>
+      </div>
+    </div>
+  </div>
+
+  <div className="col-md-4">
+    <div className="card text-white bg-danger">
+      <div className="card-body">
+        <h5>Overloaded</h5>
+        <h2>{dashboard.overloaded_sectors}</h2>
+      </div>
+    </div>
+  </div>
+
+  <div className="col-md-4">
+    <div className="card text-white bg-success">
+      <div className="card-body">
+        <h5>Average Usage</h5>
+        <h2>{dashboard.average_usage}%</h2>
+      </div>
+    </div>
+  </div>
+
+</div>
       <table className="table table-bordered table-striped">
 
         <thead className="table-dark">
